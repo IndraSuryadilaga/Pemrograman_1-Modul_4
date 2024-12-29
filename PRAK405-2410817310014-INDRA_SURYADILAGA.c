73d4b61ac0389d64aec8e8f467860fec1c4499f6
Indra Suryadilaga <indrasryd@gmail.com>
2024-12-29 17:45:34 +0800

    Update PRAK405-2410817310014-INDRA_SURYADILAGA.c

--- PRAK405-2410817310014-INDRA_SURYADILAGA.c
+++ PRAK405-2410817310014-INDRA_SURYADILAGA.c
@@ -8,21 +8,16 @@ int main() {
     for (int i = 1; i <= bil_1;  i++){
         printf("(");
         int total_baris = 0;
-
-        for(int j = i; j>= 1; j--){
+        for(int j = i; j >= 1; j--){
             printf("%d * %d", j, bil_2);
             total_baris += j * bil_2;
-
             if (j > 1){
                 printf(") + (");
             }
         }
-
         printf(") = %d\n", total_baris);
         total_semua += total_baris;
     }
-
     printf("%d\n", total_semua);
-
     return 0;
 }

7be55c705d30165705f5dba3ca155fb1cafede96
Indra Suryadilaga <indrasryd@gmail.com>
2024-12-29 17:42:59 +0800

    Update PRAK402-2410817310014-INDRA_SURYADILAGA.c

--- PRAK402-2410817310014-INDRA_SURYADILAGA.c
+++ PRAK402-2410817310014-INDRA_SURYADILAGA.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    int bilangan, bil_maksimal;
+    int bil_maksimal;
     scanf("%d", &bil_maksimal);
-    bilangan = 1;
+    int bilangan = 1;
 
     while (bilangan <= bil_maksimal) {
         if (bilangan % 2 == 1) {
             printf("%d ", bilangan);
         }
-        bilangan = bilangan + 1;
+        bilangan++;
     }
-
     printf("\n");
 
     while (bil_maksimal >= 2) {
         if (bil_maksimal % 2 ==0) {
             printf("%d ", bil_maksimal);
         }
-        bil_maksimal = bil_maksimal - 1;
+        bil_maksimal--;
     }
     return 0;
 }
